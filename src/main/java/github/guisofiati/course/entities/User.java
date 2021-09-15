5fc150b47f07358c748d63b5e79bf9e9d633c825
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2021-09-15 17:38:05 -0300

    Created order entity

--- src/main/java/github/guisofiati/course/entities/User.java
+++ src/main/java/github/guisofiati/course/entities/User.java
@@ -1,12 +1,20 @@
 package github.guisofiati.course.entities;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
-public class User {
+@Table(name = "tb_user")
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,6 +23,9 @@ public class User {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 	}
@@ -67,6 +78,10 @@ public class User {
 		this.password = password;
 	}
 
+	public List<Order> getOrders() {
+		return orders;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -91,4 +106,5 @@ public class User {
 			return false;
 		return true;
 	}
+
 }
