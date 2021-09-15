22975116a83bf7b3a156fd5888414ad8ab8aa368
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2021-09-15 15:09:53 -0300

    Project created (Boilerplate)

---
+++ src/test/java/github/guisofiati/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package github.guisofiati.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
