a061989a7c731eb64ec0c8d7da1fe9fab226603d
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2021-09-15 15:58:32 -0300

    Created user service and find user by id

--- src/main/java/github/guisofiati/course/controllers/UserController.java
+++ src/main/java/github/guisofiati/course/controllers/UserController.java
@@ -1,19 +1,33 @@
 package github.guisofiati.course.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import github.guisofiati.course.entities.User;
+import github.guisofiati.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User user = new User(1L, "Maria", "maria@gmail.com", "11999999999", "123456");
-		return ResponseEntity.ok().body(user);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
